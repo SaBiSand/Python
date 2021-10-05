e86ac665e6f7339f4201a6650fc9216f88425464
Suseth Sandoval <91089461+SaBiSand@users.noreply.github.com>
2021-10-05 15:31:44 -0500

    Add files via upload

---
+++ HolaMundo.py
@@ -0,0 +1,10 @@
+#Hola Mundo en Phython
+#Title          :Hola Mundo en Python
+#Description    :Hola Mundo en Python desde vim con Windows
+#Author         :Suseth Abigail Sandoval Damian
+#Date           :20211005
+#Version        :1
+#Usage          :c:\>python /path/HolaMundo.py
+
+
+print ("Hola mundo en Python")
